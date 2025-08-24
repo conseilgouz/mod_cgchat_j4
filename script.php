194ab55966a06b230c4bf1cd229a584841040514
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-08-24 17:07:40 +0200

    1.4.6
    Joomla 6 : getApplication()->input removed
    Joomla 6 : getDbo() removed
    Field Version : use webassets

--- script.php
+++ script.php
@@ -1,9 +1,8 @@
 <?php
 /**
 * CG Chat Module  - Joomla 4.x Module
-* Version			: 1.0.0
 * Package			: CG Chat
-* copyright 		: Copyright (C) 2023 ConseilGouz. All rights reserved.
+* copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 * From Kide ShoutBox
 */
--- src/Field/VersionField.php
+++ src/Field/VersionField.php
@@ -2,7 +2,7 @@
 /**
 * CG Chat Module  - Joomla 4.x/5.x Module
 * Package			: CG Chat
-* copyright 		: Copyright (C) 2024 ConseilGouz. All rights reserved.
+* copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
 * From Kide ShoutBox
 */
@@ -12,6 +12,7 @@ use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\FormField;
 use Joomla\CMS\Language\Text;
+use Joomla\Database\DatabaseInterface;
 use Joomla\String\StringHelper;
 
 // Prevent direct access
@@ -34,8 +35,7 @@ class VersionField extends FormField
 
 		$version = '';
 
-		$jinput = Factory::getApplication()->input;
-		$db = Factory::getDBO();
+		$db = Factory::getContainer()->get(DatabaseInterface::class);
 		$query = $db->getQuery(true);
 		$query
 			->select($db->quoteName('manifest_cache'))
@@ -46,12 +46,13 @@ class VersionField extends FormField
 		$tmp = json_decode($row['manifest_cache']);
 		$version = $tmp->version;
 		
-		$document = Factory::getDocument();
+        /** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+        $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
 		$css = '';
 		$css .= ".version {display:block;text-align:right;color:brown;font-size:10px;}";
 		$css .= ".readonly.plg-desc {font-weight:normal;}";
 		$css .= "fieldset.radio label {width:auto;}";
-		$document->addStyleDeclaration($css);
+		$wa->addInlineStyle($css);
 		$margintop = $this->def('margintop');
 		if (StringHelper::strlen($margintop)) {
 			$js = "document.addEventListener('DOMContentLoaded', function() {
@@ -59,7 +60,7 @@ class VersionField extends FormField
 			parent = vers.parentElement.parentElement;
 			parent.style.marginTop = '".$margintop."';
 			})";
-			$document->addScriptDeclaration($js);
+			$wa->addInlineScript($js);
 		}
 		$return .= '<span class="version">' . Text::_('JVERSION') . ' ' . $version . "</span>";
 
