bc269b223e62f42e2f9a34f962ecbfc59d718646
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-09-15 18:28:26 +0200

    1.4.7
    Use ConseilGouz library

--- script.php
+++ script.php
@@ -1,14 +1,92 @@
 <?php
 /**
-* CG Chat Module  - Joomla 4.x Module
+* CG Chat Module  - Joomla 4.x/5.x/6.x Module
 * Package			: CG Chat
 * copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
 * license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 * From Kide ShoutBox
 */
-defined ( '_JEXEC' ) or die ();
+defined('_JEXEC') or die();
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
 
-class Mod_CGChatInstallerScript {
-	function postflight($type, $parent) {
-	}
+class mod_cgchatInstallerScript
+{
+    private $newlib_version			 = '';
+    private $extname                 = 'cgchat';
+    private $dir           = null;
+    private $lang;
+
+    public function __construct()
+    {
+        $this->dir = __DIR__;
+        $this->lang = Factory::getApplication()->getLanguage();
+        $this->lang->load($this->extname);
+    }
+
+    public function postflight($type, $parent)
+    {
+
+        if (!$this->checkLibrary('conseilgouz')) { // need library installation
+            $ret = $this->installPackage('lib_conseilgouz');
+            if ($ret) {
+                Factory::getApplication()->enqueueMessage('ConseilGouz Library ' . $this->newlib_version . ' installed', 'notice');
+            }
+        }
+        // delete obsolete version.php file
+        $this->delete([
+            JPATH_SITE . '/modules/mod_cgchat/src/Field/VersionField.php',
+        ]);
+
+    }
+    private function checkLibrary($library)
+    {
+        $file = $this->dir.'/lib_conseilgouz/conseilgouz.xml';
+        if (!is_file($file)) {// library not installed
+            return false;
+        }
+        $xml = simplexml_load_file($file);
+        $this->newlib_version = $xml->version;
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $conditions = array(
+             $db->qn('type') . ' = ' . $db->q('library'),
+             $db->qn('element') . ' = ' . $db->quote($library)
+            );
+        $query = $db->getQuery(true)
+                ->select('manifest_cache')
+                ->from($db->quoteName('#__extensions'))
+                ->where($conditions);
+        $db->setQuery($query);
+        $manif = $db->loadObject();
+        if ($manif) {
+            $manifest = json_decode($manif->manifest_cache);
+            if ($manifest->version >= $this->newlib_version) { // compare versions
+                return true; // library ok
+            }
+        }
+        return false; // need library
+    }
+    private function installPackage($package)
+    {
+        $tmpInstaller = new Joomla\CMS\Installer\Installer();
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $tmpInstaller->setDatabase($db);
+        $installed = $tmpInstaller->install($this->dir . '/' . $package);
+        return $installed;
+    }
+    public function delete($files = [])
+    {
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                Folder::delete($file);
+            }
+
+            if (is_file($file)) {
+                File::delete($file);
+            }
+        }
+    }
+
 }
